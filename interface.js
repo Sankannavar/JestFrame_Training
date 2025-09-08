ce7a6526f5bc5af43f3df7e0423dcb7fabb34817
Sankannavar <sankannavarchetana@gmail.com>
2025-09-08 13:02:27 +0530

    Day1

---
+++ interface.js
@@ -0,0 +1,5 @@
+var emp1 = {
+    name: "chetana",
+    age: 30
+};
+console.log(emp1);
---
+++ interface.ts
@@ -0,0 +1,11 @@
+interface Employee{
+    name:string;
+    age:number;
+}
+
+let emp1: Employee = {
+    name:"chetana",
+    age:30
+} 
+
+console.log(emp1);
