e3979071a70d8ac075dff7ce7948b089334e3f86
Sankannavar <sankannavarchetana@gmail.com>
2025-09-10 20:40:14 +0530

    MiniProject

---
+++ src/logger.ts
@@ -0,0 +1,11 @@
+export class Logger{
+    log(message:string):void{
+        console.log('[LOG]:${message}');
+    }
+
+    error(message:string):void{
+        console.error('[ERROR]: ${message}');
+    }
+}
+export const logger=new Logger();
+}
