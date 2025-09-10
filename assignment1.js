8df4582deb4e23ef824bf8f11fd2c911b2cc2cec
Sankannavar <sankannavarchetana@gmail.com>
2025-09-10 12:11:57 +0530

    removed assignment

--- assignment1.js
+++
@@ -1,8 +0,0 @@
-var studentName = "Chetana";
-var age = 30;
-var enrolled = true;
-var cources = ["Typescript", "Jest Framework"];
-function getStudentInfo(name, age, enrolled) {
-    return "student ".concat(name, " is ").concat(age, " years old enrolled:").concat(enrolled);
-}
-console.log(getStudentInfo(studentName, age, enrolled));
