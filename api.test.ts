b133b7a794221b1d5e74b63e4bf400434928113c
Sankannavar <sankannavarchetana@gmail.com>
2025-09-11 13:48:06 +0530

    calllback

--- api.test.ts
+++ api.test.ts
@@ -20,3 +20,11 @@ test('fetches user data successfully', async () => {
 //     expect(data).toEqual({ id: 1, name: "John Doe" });
 //     expect(fetchMock).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/users1");
 // });
+
+test('fetches user data successfully1', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ id: 1, title: "delectus aut autem" ,completed:false}));
+
+    const data = await fetchData("1");
+    expect(fetchMock).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos/1");
+    expect(data).toEqual({ id: 1, title: "delectus aut autem",completed:false });
+});
