255a0970c463c908cb16cac5834c233af54d7f9a
Sankannavar <sankannavarchetana@gmail.com>
2025-09-12 11:47:00 +0530

    cleanProject

--- Promise.ts
+++
@@ -1,6 +0,0 @@
-function fetchData():Promise<string>{
-    return new Promise((resolve)=>{
-    setTimeout(()=>{
-    resolve("Hello");},2000);
-    });
-}
--- promise.js
+++
@@ -1,7 +0,0 @@
-function fetchData() {
-    return new Promise(function (resolve) {
-        setTimeout(function () {
-            resolve("Hello");
-        }, 2000);
-    });
-}
